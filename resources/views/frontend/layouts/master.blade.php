86e00eabacaf2928ffac5fe84b13ac87ab975a15
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-08-23 10:00:08 +0700

    Details page: Adding dynamic Metas
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -4,8 +4,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>{{ __('Top News') }}</title>
-    <meta name="description" content="">
+    <title>@yield('title')</title>
+    <meta name="description" content="@yield('meta_description')">
+    <meta name="og:title" content="@yield('meta_og_title')" />
+    <meta name="og:description" content="@yield('meta_og_description')" />
+    <meta name="og:image" content="@yield('meta_og_image')" />
+    <meta name="twitter:title" content="@yield('meta_tw_title') "/>
+    <meta name="twitter:description" content="@yield('meta_tw_description')" />
+    <meta name="twitter:image" content="@yield('meta_tw_image')" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset('frontend/asset/css/styles.css') }}" rel="stylesheet">
 </head>
